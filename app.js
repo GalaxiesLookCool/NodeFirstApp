b1e9967bfe3e473c8dc310fa49a0a1826f8f039f
GalaxiesLookCool <sporeofficer@gmail.com>
2023-07-17 21:00:22 +0300

    after installing packages

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log("hello!");
+
+const { exec } = require('node:child_process')
+const { spawn } = require('node:child_process')
+
+const http = require('http');
+const server = http.createServer();
+
+const io = require('socket.io')(server);
+
+
+
+function runCommand(command) {
+    exec(command, (err, output) => {
+        if (err) {
+            return err;
+        }
+        return output;
+    })
+}
+
+socketsArr = new Array(); 
+
+io.on('connection', (socket) => {
+    console.log("connection acquired!");
+    socketsArr.push(socket)
+
+    socket.on('disconnect', () => {
+        console.log("connection from " + socket.handshake.address + " disconnected");
+        socketsArr.splice(socketsArr.indexOf(socket), 1);
+    })
+    
+})
+
+function sleep(time) {
+    var waitTill = new Date(new Date().getTime() + time );
+while(waitTill > new Date()){}
+}
